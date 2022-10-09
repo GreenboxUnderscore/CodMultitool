88f5c2a7a8ee5773f7e317ac97f23fb440d9366f
GreenboxUnderscore <greenboxunderscore@gmail.com>
2022-10-09 19:23:58 -0400

    changed "tier >= 1" to "tier < 1"

--- src/main/java/net/codmultitool/items/tools/CodTool.java
+++ src/main/java/net/codmultitool/items/tools/CodTool.java
@@ -18,7 +18,7 @@ public class CodTool extends PickaxeItem {
         } else if (tier < 2 && blockstate.isIn(BlockTags.NEEDS_IRON_TOOL)) {
             return false;
         } else {
-            return (tier >= 1 || !blockstate.isIn(BlockTags.NEEDS_STONE_TOOL)) && (blockstate.isIn(BlockTags.AXE_MINEABLE) || blockstate.isIn(BlockTags.HOE_MINEABLE)
+            return (tier < 1 || !blockstate.isIn(BlockTags.NEEDS_STONE_TOOL)) && (blockstate.isIn(BlockTags.AXE_MINEABLE) || blockstate.isIn(BlockTags.HOE_MINEABLE)
                     || blockstate.isIn(BlockTags.PICKAXE_MINEABLE) || blockstate.isIn(BlockTags.SHOVEL_MINEABLE));
         }
     }
